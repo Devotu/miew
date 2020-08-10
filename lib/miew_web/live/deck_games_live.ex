ad7d1260aee4903ea0ccd814012335eae8ff3b95
DevotuOne <devotu.developer@gmail.com>
2020-08-11 00:03:25 +0200

    Deck - View winrate

--- lib/miew_web/live/deck_games_live.ex
+++ lib/miew_web/live/deck_games_live.ex
@@ -10,17 +10,27 @@ defmodule MiewWeb.DeckGamesLive do
     deck_id = params["id"]
 
     games = Metr.list_games(:deck, deck_id)
-      |> IO.inspect(label: "deck games live - input")
      |> Enum.sort(&(&1.time < &2.time))
      |> Enum.map(fn g -> {find_selected(g.participants, deck_id), find_opponent(g.participants, deck_id)} end)
      |> IO.inspect(label: "deck games live - self/opponent")
-     |> Enum.map(fn {selected, opponent} ->
+     |> Enum.reduce(
+       %{games: [], game_count: 0, win_count: 0},
+       fn {selected, opponent}, acc -> %{
+         games: acc.games ++ [{selected, opponent, calculate_winrate(inc(acc.game_count), add_win(acc.win_count, selected.place))}],
+         game_count: inc(acc.game_count),
+         win_count: add_win(acc.win_count, selected.place) }
+        end
+       )
+     |> IO.inspect(label: "deck games live - acc")
+     |> Map.get(:games)
+     |> IO.inspect(label: "deck games live - games")
+     |> Enum.map(fn {selected, opponent, winrate} ->
       %DeckGamesLive{
         vs_deck: opponent.deck_id,
         vs_player: opponent.player_id,
         power: selected.power,
         fun: selected.fun,
-        z_winrate: 0,
+        z_winrate: winrate,
         z_power: 0,
         z_fun: 0
       }
@@ -32,6 +42,16 @@ defmodule MiewWeb.DeckGamesLive do
     {:ok, assign(socket, games: games)}
   end
 
+
+  defp add_win(current, 1), do: inc(current)
+  defp add_win(current, _place), do: current
+
+  defp inc(count), do: count + 1
+
+  defp calculate_winrate(games, wins) do
+    wins/games * 100
+    |> round()
+  end
 
   defp find_selected(participants, deck_id) do
     participants
