87c596bba7e21292ef72387174a0929e4ea4459f
DevotuOne <devotu.developer@gmail.com>
2020-08-17 23:19:58 +0200

    Player - Add

--- lib/miew_web/live/new_player_live.ex
+++ lib/miew_web/live/new_player_live.ex
@@ -1,8 +1,13 @@
 defmodule MiewWeb.NewPlayerLive do
   use MiewWeb, :live_view
 
+  alias Metr
+
   def render(assigns) do
     ~L"""
+    <section class="phx-hero">
+      <p><%= assigns.name_added %>
+    </section>
     <section class="phx-hero">
       <form phx-submit="add">
         <input type="text" name="name" value="<%= @name %>" placeholder="Name"/>
@@ -14,14 +19,14 @@ defmodule MiewWeb.NewPlayerLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, name: "")}
+    {:ok, assign(socket, name: "", name_added: "")}
   end
 
 
   @impl true
   @spec handle_event(<<_::24>>, map, any) :: {:noreply, any}
   def handle_event("add", %{"name" => name}, socket) do
-    IO.puts(name)
-    {:noreply, socket}
+    player_id = Metr.create_player(%{name: name})
+    {:noreply, assign(socket, name_added: "Added: " <> name)}
   end
 end
