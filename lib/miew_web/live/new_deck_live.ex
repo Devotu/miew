aadbdb6c7583ce3021381a6386743bbd0f00c366
DevotuOne <devotu.developer@gmail.com>
2020-10-19 23:53:08 +0200

    Refactor - Remove unused IO

--- lib/miew_web/live/new_deck_live.ex
+++ lib/miew_web/live/new_deck_live.ex
@@ -16,8 +16,6 @@ defmodule MiewWeb.NewDeckLive do
     players = Enum.map(Metr.list_players(), fn p -> p.id end)
     formats = Metr.list_formats()
 
-    IO.puts(Kernel.inspect(players))
-
     {:ok, assign(socket,
     owner: "",
     name: "", format: "", theme: "",
