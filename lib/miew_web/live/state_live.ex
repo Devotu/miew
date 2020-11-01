432b8088a5f86bd681ab32e36538c826ef79956e
DevotuOne <devotu.developer@gmail.com>
2020-11-01 14:14:02 +0100

    State - Read state of x

--- lib/miew_web/live/state_live.ex
+++ lib/miew_web/live/state_live.ex
@@ -1,4 +1,4 @@
-defmodule MiewWeb.LogLive do
+defmodule MiewWeb.StateLive do
   use MiewWeb, :live_view
 
   alias Metr
@@ -7,13 +7,7 @@ defmodule MiewWeb.LogLive do
   def render(assigns) do
     ~L"""
     <section class="phx-hero">
-      <table>
-        <%= for event <- @events do %>
-        <tr>
-          <td><%= Kernel.inspect(event) %></td>
-        </tr>
-        <% end %>
-      </table>
+      <%= Kernel.inspect(@state) %>
     </section>
     """
   end
@@ -22,7 +16,7 @@ defmodule MiewWeb.LogLive do
   def mount(params, _session, socket) do
     type = params["type"] |> Metr.type_from_string()
     id = params["id"]
-    log = Metr.read_entity_log(type, id)
-    {:ok, assign(socket, events: log)}
+    player_state = Metr.read_state(type, id)
+    {:ok, assign(socket, state: player_state)}
   end
 end
