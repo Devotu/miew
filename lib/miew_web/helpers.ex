6bc10edf27e5ceca0a20cfa108470ac4c13964b1
DevotuOne <devotu.developer@gmail.com>
2020-11-17 22:56:49 +0100

    Batch - Load data

--- lib/miew_web/helpers.ex
+++ lib/miew_web/helpers.ex
@@ -1,10 +1,9 @@
 defmodule Miew.Helpers do
 
   def text_to_bool(nil), do: false
-  def text_to_bool(term) when is_bitstring(term) do
-    case term do
-      "true" -> true
-      "false" -> false
-    end
-  end
+  def text_to_bool(""), do: false
+  def text_to_bool("true"), do: true
+  def text_to_bool("false"), do: false
+  def text_to_bool("1"), do: true
+  def text_to_bool("0"), do: false
 end
--- lib/miew_web/live/parse_decks_live.ex
+++ lib/miew_web/live/parse_decks_live.ex
@@ -1,7 +1,7 @@
 defmodule MiewWeb.ParseDecksLive do
   use MiewWeb, :live_view
 
-  alias Metr
+  alias Miew.Helpers
 
   @default_deck %{
     name: "name", format: "format", theme: "tema",
@@ -17,9 +17,13 @@ defmodule MiewWeb.ParseDecksLive do
 
 
   @impl true
-  def handle_event("parse", %{} = data, socket) do
-    output = [@default_deck]
-    {:noreply, assign(socket, decks: output)}
+  def handle_event("parse", %{"data" => data}, socket) do
+    parsed = data
+      |> String.split(",")
+      |> Enum.map(&(String.split(&1, ";")))
+      |> Enum.map(&pick_apart/1)
+
+    {:noreply, assign(socket, decks: parsed)}
   end
 
 
@@ -27,5 +31,21 @@ defmodule MiewWeb.ParseDecksLive do
   def handle_event("apply", %{} = data, socket) do
     msg = "Här kommer output att vara"
     {:noreply, assign(socket, msg: msg)}
+  end
+
+
+  defp pick_apart([cost, format, name, creator, red, green, white, black, blue]) do
+    %{
+      name: name,
+      format: String.downcase(format),
+      creator: creator,
+      black: Helpers.text_to_bool(black),
+      white: Helpers.text_to_bool(white),
+      red: Helpers.text_to_bool(red),
+      green: Helpers.text_to_bool(green),
+      blue: Helpers.text_to_bool(blue),
+      colorless: false,
+      cost: cost
+    }
   end
 end
