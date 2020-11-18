16307daef56570c0b77c31fbf91bef2d0f3214bc
DevotuOne <devotu.developer@gmail.com>
2020-11-18 22:46:52 +0100

    Helpers - Text to number

--- lib/miew_web/helpers.ex
+++ lib/miew_web/helpers.ex
@@ -6,4 +6,14 @@ defmodule Miew.Helpers do
   def text_to_bool("false"), do: false
   def text_to_bool("1"), do: true
   def text_to_bool("0"), do: false
+
+  def text_to_number(nil), do: 0
+  def text_to_number(""), do: 0
+  def text_to_number(term) when is_number(term), do: term
+  def text_to_number(term) do
+    case Float.parse(term) do
+      {x, ""} -> x
+      :error -> {:error, "#{term} is not a number"}
+    end
+  end
 end
