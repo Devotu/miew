8d78b23c2c903d6af7b84ad8838dceef3b44e1a3
DevotuOne <devotu.developer@gmail.com>
2020-11-30 23:03:24 +0100

    v 0.8.0 - Batch Games

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Miew.MixProject do
   def project do
     [
       app: :miew,
-      version: "0.7.1",
+      version: "0.8.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
