c99b5254950264e7b94ed92307dec971892903f9
Unknown <devotu.developer@gmail.com>
2021-03-21 21:59:54 +0100

    Script - upload - phx release

---
+++ deployment/upload.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+echo -e "version"
+read version
+
+echo "---- application ----"
+echo "---- fetching dependancies"
+mix deps.get --only prod
+echo "---- dependancies done"
+
+echo "---- compiling application"
+MIX_ENV=prod mix compile
+echo "---- application compiled"
+echo "---- application done --"
+
+echo "---- assets ----"
+echo "---- updating js depencancies"
+npm install --prefix ./assets
+echo "---- update done"
+
+echo "---- compiling assets"
+npm run deploy --prefix ./assets
+echo "---- assets compiled"
+mix phx.digest
+echo "---- assets done --"
+
+echo "---- release ----"
+echo "---- building release"
+MIX_ENV=prod mix release
+echo "---- release built"
+echo "---- release done"
+
+
+#zippa med version i namn
+
+echo "release $version built and zipped"
+
+#läs in ip
+#läs in pwd
+#scp till server
+
+echo "done"
