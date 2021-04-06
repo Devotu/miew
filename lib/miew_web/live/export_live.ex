e1995311334266a33584a383acf7fefc259af634
Ottu <otuvesson@gmail.com>
2021-04-06 23:43:30 +0200

    Export - Refactor

--- lib/miew_web/live/export_live.ex
+++ lib/miew_web/live/export_live.ex
@@ -41,18 +41,18 @@ defmodule MiewWeb.ExportLive do
   @impl true
   def handle_event("decks", _data, socket) do
     output = Miew.list("deck")
-      |> convert()
+      |> convert_decks()
     {:noreply, assign(socket, output: output)}
   end
 
-  defp convert(decks) when is_list(decks) do
+  defp convert_decks(decks) when is_list(decks) do
     players = Miew.list("player")
     decks
     |> Enum.map(fn d -> merge_owner(d, players) end)
-    |> Enum.map(&convert/1)
+    |> Enum.map(&convert_deck/1)
   end
 
-  defp convert(d) when is_map(d) do
+  defp convert_deck(d) when is_map(d) do
     """
     #{d.price};#{d.format};#{d.name};#{d.player_id};#{d.theme};#{bn(d.red)};#{bn(d.green)};#{bn(d.white)};#{bn(d.black)};#{bn(d.blue)},
     """
