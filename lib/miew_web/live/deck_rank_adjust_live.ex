adffc42b109e5c207c213558a88903eb1efd6f8e
Ottu <otuvesson@gmail.com>
2021-04-22 21:20:47 +0200

    Deck - Adjust rank - CSS

--- lib/miew_web/live/deck_rank_adjust_live.ex
+++ lib/miew_web/live/deck_rank_adjust_live.ex
@@ -7,27 +7,30 @@ defmodule MiewWeb.DeckRankAdjustLive do
   @impl true
   def render(assigns) do
     ~L"""
-    <section>
-      <table>
-        <tr>
-          <th>Name</td>
-          <th>Rank</td>
-          <th>Advantage</td>
-          <th>+</td>
-          <th>-</td>
-        </tr>
-        <tr>
-          <td><%= @deck.name %></td>
-          <td><%= @deck.rank %></td>
-          <td><%= @deck.advantage %></td>
-          <td>
-            <button phx-click="up" phx-value-deck_id=<%= @deck.id %>>+</button>
-          </td>
-          <td>
-            <button phx-click="down" phx-value-deck_id=<%= @deck.id %>>-</button>
-          </td>
-        </tr>
-      </table>
+    <section class="plaque">
+      <div>
+        <ul class="v-list header">
+          <li class="modifyrankrow label">
+            <span class="v-list-item">Name</span>
+            <span class="v-list-item">Rank</span>
+            <span class="v-list-item">Advantage</span>
+          </li>
+        </ul>
+        <ul class="v-list header">
+          <li class="modifyrankrow">
+            <span class="v-list-item"><%= @deck.name %></span>
+            <span class="v-list-item"><%= @deck.rank %></span>
+            <span class="v-list-item"><%= @deck.advantage %></span>
+          </li>
+        </ul>
+        <div class="flex flex-spread row append-b">
+          <%= if @changes > 0 do %>
+          <button class="flexi box-fill" type="button" color="red" phx-click="switch_sure">Do not end</button>
+          <% end %>
+          <button class="flexi box-fill right" type="button" phx-click="down" phx-value-deck_id=<%= @deck.id %>>-</button>
+          <button class="flexi box-fill right" type="button" phx-click="up" phx-value-deck_id=<%= @deck.id %>>+</button>
+        </div>
+      </div>
     </section>
     """
   end
@@ -36,7 +39,7 @@ defmodule MiewWeb.DeckRankAdjustLive do
   def mount(params, _session, socket) do
     deck = params["id"]
       |> load_deck
-    {:ok, assign(socket, deck: deck)}
+    {:ok, assign(socket, deck: deck, changes: 0)}
   end
 
 
@@ -54,6 +57,17 @@ defmodule MiewWeb.DeckRankAdjustLive do
     Miew.bump_rank(id, :down)
     deck = load_deck(id)
     {:noreply, assign(socket, deck: deck)}
+  end
+
+
+  @impl true
+  def handle_event("confirm", _data, socket) do
+    case socket.assigns.sure do
+      true ->
+        {:noreply, assign(socket, sure: !socket.assigns.sure)}
+      false ->
+        {:noreply, assign(socket, sure: !socket.assigns.sure)}
+    end
   end
 
   def load_deck(id) do
