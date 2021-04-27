fad463bc598c09a833aa9245ba0d11ef1f97e564
Ottu <otuvesson@gmail.com>
2021-04-27 22:33:11 +0200

    Refactor - Purge unused

--- lib/miew_web/router.ex
+++ lib/miew_web/router.ex
@@ -25,7 +25,6 @@ defmodule MiewWeb.Router do
     live "/deck/new", NewDeckLive
     live "/deck/list", DeckListLive
     live "/deck/:id", DeckLive
-    live "/deck/:id/games", DeckGamesLive
     live "/deck/:id/results", DeckResultsLive
     live "/deck/:id/rank/adjust", DeckRankAdjustLive
 
