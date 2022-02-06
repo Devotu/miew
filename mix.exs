9dbc7fb43834476ce10beac4aaabdcefb0cd9964
Ottu <otuvesson@gmail.com>
2022-02-07 00:21:21 +0100

    Mix - Bump to latest
    phoenix 1.6.0
    live view 0.17.6

--- mix.exs
+++ mix.exs
@@ -33,12 +33,12 @@ defmodule Miew.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.3"},
-      {:phoenix_live_view, "~> 0.13.0"},
-      {:floki, ">= 0.0.0", only: :test},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix, "~> 1.6.0"},
+      {:phoenix_live_view, "~> 0.17.6"},
+      {:floki, ">= 0.30.0", only: :test},
+      {:phoenix_html, "~> 3.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix_live_dashboard, "~> 0.6.4"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
